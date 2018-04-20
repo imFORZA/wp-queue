3ce3e7fa37d8ad44f3c89dc2b2d04b8481e8d35c
Brandon Hubbard <bkhubbard@gmail.com>
2018-04-20 12:26:50 -0700

    removed category from job itself, still custom column

--- src/WP_Queue/Job.php
+++ src/WP_Queue/Job.php
@@ -11,11 +11,6 @@ abstract class Job {
 	 * @var int
 	 */
 	private $id;
-	
-	/**
-	 * @var Category
-	 */
-	private $category;
 
 	/**
 	 * @var int
@@ -68,15 +63,6 @@ abstract class Job {
 	 */
 	public function set_id( $id ) {
 		$this->id = $id;
-	}
-	
-	/**
-	 * Set Job Category.
-	 *
-	 * @param text $category Category.
-	 */
-	public function set_category( $category ) {
-		$this->category = $category;
 	}
 
 	/**
