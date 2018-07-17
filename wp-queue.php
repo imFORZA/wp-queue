21f3ff694b155266dc4e017c7c86c3d722f320bb
Santiago Garza <santiago.garza@imforza.com>
2018-07-17 13:29:13 -0700

    Add Brandons Changes

---
+++ wp-queue.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ ----------------------------------------------------------------------------------------------------------------------
+  Plugin Name: WP Queue
+  Version: 0.0.1
+  Plugin URI:
+  Description: A plugin for background processes
+  Author: Brandon Hubbard
+  Author URI: https://brandonhubbard.com
+  Text Domain: wpqueue-restapi
+  License: GPL v3
+  License URI: https://www.gnu.org/licenses/gpl-3.0.html
+
+ ----------------------------------------------------------------------------------------------------------------------
+*/
+
+include_once('src/functions.php');
+include_once('src/rest-api.php');
+include_once('src/cli-commands.php');
+
+include_once('src/debug-page.php');
+include_once('src/failure-page.php');
+
+wp_queue_options();
