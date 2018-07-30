30f575613b4be22fc9dedae25909fc4f0b91c6cd
Brandon Hubbard <bhubbard@users.noreply.github.com>
2018-07-30 09:33:03 -0700

    Cleanup of WordPress Header

--- wp-queue.php
+++ wp-queue.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * WP_Queue library.
+ * WP_Queue Library.
  *
  * @package WP_Queue
  */
@@ -11,9 +11,9 @@ Plugin Name: WP Queue
 Version: 0.0.1
 Plugin URI:
 Description: A plugin for background processes
-Author: Brandon Hubbard
-Author URI: https://brandonhubbard.com
-Text Domain: wpqueue-restapi
+Author: The WP Queue Team.
+Author URI: https://www.wp-queue.com
+Text Domain: wp-queue
 License: GPL v3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 ----------------------------------------------------------------------------------------------------------------------
