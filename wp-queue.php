6bda43633bc00715fe8a9314442499b23e1fd73b
Santiago Garza <santiago.garza@imforza.com>
2018-08-04 09:16:16 -0700

    Bumping version number

--- wp-queue.php
+++ wp-queue.php
@@ -10,7 +10,7 @@ use WP_Queue\QueueManager;
 /*
 ---------------------------------------------------------------------------------------------------------------------
 Plugin Name: WP Queue
-Version: 1.0.0
+Version: 2.0.0
 Plugin URI:
 Description: A plugin for background processes
 Author: The WP Queue Team.
